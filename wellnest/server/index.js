b68a2490f8db66c2d9c9feab95a92954361993a3
bensuz <elifbensuaslan@gmail.com>
2023-08-18 14:44:23 +0100

    version before deployment

---
+++ wellnest/server/index.js
@@ -0,0 +1,12 @@
+require("dotenv/config");
+const express = require(express);
+const cors = require(cors);
+const app = express();
+const connectDB = require("./config/databs");
+const PORT = process.env.PORT || 8000;
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("Server is running on port:", PORT);
+    });
+});
--- wellnest/server/models/user.js
+++ wellnest/server/models/user.js
@@ -32,5 +32,38 @@ const userSchema = mongoose.Schema(
     }
 );
 
+//middlewares for user authentication
+
+//virtual because we do not want same password in our DB twice, this will be only for confirmation in virtual
+userSchema
+    .virtual("confirmPassword")
+    .get(() => this._confirmPassword)
+    .set((value) => (this._confirmPassword = value));
+
+//before the validation run (pre), we want to check if the passwords match
+//we cannot have arrow function bcs we want access to this keyword
+//we put next bcs it's a middleware
+//only runs for validation
+userSchema.pre("validate", function (next) {
+    if (this.password !== this.confirmPassword) {
+        this.invalidate("confirmPassword", "Passwords need to match!"); // when you invalidate you need to provide key and message
+    }
+    next();
+});
+
+//before saving (pre) we are hashing the password
+//only runs for save
+userSchema.pre("save", async function (next) {
+    try {
+        const hashedPassword = await bcrypt.hash(this.password, 6); //will be encrypted in 6 rounds
+        console.log("Hashed Password:", hashedPassword);
+        console.log("Password:", this.password);
+        this.password = hashedPassword;
+    } catch (error) {
+        console.log("Hashing error:", error);
+    }
+    next();
+});
+
 const model = mongoose.model("User", userSchema);
 module.exports = model;
