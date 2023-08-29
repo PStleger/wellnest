9e8e34591dc2e4c185a70526aa2245ef50c16035
Keerthana-Prabhakar <109957611+Keerthana-Prabhakar@users.noreply.github.com>
2023-08-29 16:38:24 +0200

    footer

--- client/src/components/HowItWorks.jsx
+++ client/src/components/HowItWorks.jsx
@@ -1,5 +1,43 @@
+import './HowItWorks.css';
+
 const HowItWorks = () => {
-    return <div></div>;
+    return (
+        <div className="how-it-works">
+            <div className="hero">
+                <h1>How Our App Works</h1>
+                <p>Discover the steps to achieve better mental well-being.
+                Discover the Power of Emotion with WellNest
+                </p>
+            </div>
+            <div className="steps">
+                <div className="step">
+                    <h2>Step 1: Sign Up</h2>
+                    <p>Create an account to access personalized content.</p>
+                </div>
+                <div className="step">
+                    <h2>Step 2: Assess Your Well-being</h2>
+                    <p>Answer a few questions to gauge your mental state.</p>
+                </div>
+                <div className="step">
+                    <h2>Step 3: Start your body scan</h2>
+                    <p>The body scan can make you feel better</p>
+                </div>
+                <div className="step">
+                    <h2>Step 4: Answer the questions again</h2>
+                    <p>Answer the questions to see what has changed.</p>
+                </div>
+                <div className="step">
+                    <h2>Step 5: Write your own Journals</h2>
+                    <p>Start writting how you feel everyday and find out what is draining your energy.</p>
+                </div>
+                <div className="step">
+                    <h2>Step 6: Refer to other Articles that might help you.</h2>
+                    <p>Just so you know how others deal with it.</p>
+                </div>
+            
+            </div>
+        </div>
+    );
 };
 
 export default HowItWorks;
