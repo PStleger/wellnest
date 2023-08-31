268f8f7e1117750b98dd007cf05ef2ac02de0c15
Phoebe St Leger-Davey <phoebestld@gmail.com>
2023-08-31 13:09:24 +0100

    getalljournals

--- client/src/components/NewJournal.jsx
+++ client/src/components/NewJournal.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import ReactQuill from "react-quill";
+import { Link } from "react-router-dom";
 import "react-quill/dist/quill.snow.css";
 import { useNavigate } from "react-router-dom";
 import axios from "../axiosInstance";
@@ -54,15 +55,15 @@ const NewJournal = () => {
                   </label>
                 </div>
 
-                <button
-                  href="#_"
+                <Link
+                  to="../journals"
                   onClick={handleNewJournal}
                   className="relative inline-flex items-center justify-center p-4 px-5 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-2xl shadow-xl group hover:ring-1 hover:ring-purple-500"
                 >
                   <span className="absolute inset-0 w-full h-full bg-gradient-to-br from-[#88dfee] via-purple-400 to-[#DFC6E0]"></span>
                   <span className="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-45 translate-x-24 bg-pink-500 rounded-full opacity-30 group-hover:rotate-90 ease"></span>
                   <span className="relative text-white">Add Entry</span>
-                </button>
+                </Link>
               </div>
             </div>
           </div>
