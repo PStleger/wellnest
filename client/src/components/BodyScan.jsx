27f16e97f15d55466663932dfda8cb4ac9f351de
Gisella Haladin <gisellahaladin@gmail.com>
2023-09-02 21:15:07 +0200

    working on

---
+++ client/src/components/BodyScan.jsx
@@ -0,0 +1,173 @@
+import "./BodyScan.css";
+import image2 from "../assets/3363303.jpg";
+
+const BodyScan = () => {
+    return (
+        <div className="flex flex-col justify-center items-center">
+            <div className="hwrap">
+                <div className="hmove">
+                    <div className="hslide">
+                        <div className="flex flex-row justify-center items-center">
+                            <p className="text-[#6C1770] p-8 ml-5 font-bold text-2xl text-center max-w-md animate-fade animate-once animate-duration-[3000ms] animate-delay-0 animate-ease-linear animate-normal animate-fill-forwards">
+                                <span className="font-extrabold">
+                                    Begin with Breath Awareness: <br />
+                                    <br />
+                                </span>
+                                Close your eyes and take a few deep, slow
+                                breaths to center yourself. Pay attention to the
+                                sensation of the breath entering and leaving
+                                your body. This helps you transition into a
+                                state of mindfulness.
+                            </p>
+
+                            <img
+                                className="w-[500px] mix-blend-multiply p-8"
+                                src={image2}
+                                alt=""
+                            />
+                        </div>
+                    </div>
+                    <div className="hslide">
+                        <div className="flex flex-row justify-center items-center">
+                            <p className="text-[#6C1770] p-8 font-bold text-2xl text-center max-w-md animate-fade animate-once animate-duration-[3000ms] animate-delay-0 animate-ease-linear animate-normal animate-fill-forwards">
+                                <span className="font-extrabold">
+                                    Start at Your Feet: <br />
+                                    <br />
+                                </span>
+                                Direct your focus to your feet. Notice any
+                                sensations, warmth, coolness, tingling, or
+                                tension. Pay attention without judgment. Simply
+                                observe what you feel.
+                            </p>
+                            <img
+                                className="w-[500px] mix-blend-multiply p-8"
+                                src={image2}
+                                alt=""
+                            />
+                        </div>
+                    </div>
+                    <div className="hslide">
+                        <div className="flex flex-row justify-center items-center">
+                            <p className="text-[#6C1770] p-8 font-bold text-2xl text-center max-w-md animate-fade animate-once animate-duration-[3000ms] animate-delay-0 animate-ease-linear animate-normal animate-fill-forwards">
+                                <span className="font-extrabold">
+                                    Progress Slowly Upwards: <br />
+                                    <br />
+                                </span>
+                                Gradually move your attention upward, through
+                                your ankles, calves, knees, thighs, and so on.
+                                At each area, spend a moment observing any
+                                sensations or tensions. If you notice any
+                                discomfort or tightness, acknowledge it without
+                                trying to change it.
+                            </p>
+                            <img
+                                className="w-[500px] mix-blend-multiply p-8"
+                                src={image2}
+                                alt=""
+                            />
+                        </div>
+                    </div>
+                    <div className="hslide">
+                        <div className="flex flex-row justify-center items-center">
+                            <p className="text-[#6C1770] p-8 font-bold text-2xl text-center max-w-md animate-fade animate-once animate-duration-[3000ms] animate-delay-0 animate-ease-linear animate-normal animate-fill-forwards">
+                                <span className="font-extrabold">
+                                    Scan Your Torso and Back: <br />
+                                    <br />
+                                </span>
+                                Continue scanning through your pelvis, abdomen,
+                                lower back, upper back, and chest. Be attentive
+                                to the sensations within your body. Are there
+                                areas of ease or discomfort? Are there emotions
+                                associated with these sensations?
+                            </p>
+                            <img
+                                className="w-[500px] mix-blend-multiply p-8"
+                                src={image2}
+                                alt=""
+                            />
+                        </div>
+                    </div>
+                    <div className="hslide">
+                        <div className="flex flex-row justify-center items-center">
+                            <p className="text-[#6C1770] p-8 font-bold text-2xl text-center max-w-md animate-fade animate-once animate-duration-[3000ms] animate-delay-0 animate-ease-linear animate-normal animate-fill-forwards">
+                                <span className="font-extrabold">
+                                    Move to Your Arms: <br />
+                                    <br />
+                                </span>
+                                Shift your attention to your arms, starting with
+                                your fingers, hands, wrists, forearms, and upper
+                                arms. Notice any tingling, warmth, or tension in
+                                these areas.
+                            </p>
+                            <img
+                                className="w-[500px] mix-blend-multiply p-8"
+                                src={image2}
+                                alt=""
+                            />
+                        </div>
+                    </div>
+                    <div className="hslide">
+                        <div className="flex flex-row justify-center items-center">
+                            <p className="text-[#6C1770] p-8 font-bold text-2xl text-center max-w-md animate-fade animate-once animate-duration-[3000ms] animate-delay-0 animate-ease-linear animate-normal animate-fill-forwards">
+                                <span className="font-extrabold">
+                                    Scan Your Neck and Head: <br />
+                                    <br />
+                                </span>
+                                Gradually move up to your neck and head,
+                                including your face and scalp. Pay close
+                                attention to any sensations, even if they seem
+                                subtle.
+                            </p>
+                            <img
+                                className="w-[500px] mix-blend-multiply p-8"
+                                src={image2}
+                                alt=""
+                            />
+                        </div>
+                    </div>
+                    <div className="hslide">
+                        <div className="flex flex-row justify-center items-center">
+                            <p className="text-[#6C1770] p-8 font-bold text-2xl text-center max-w-md animate-fade animate-once animate-duration-[3000ms] animate-delay-0 animate-ease-linear animate-normal animate-fill-forwards">
+                                <span className="font-extrabold">
+                                    Overall Body Awareness: <br />
+                                    <br />
+                                </span>
+                                Once you've scanned your entire body, take a few
+                                moments to expand your awareness to your body as
+                                a whole. Feel the connection between different
+                                parts of your body. Observe your overall sense
+                                of well-being.
+                            </p>
+                            <img
+                                className="w-[500px] mix-blend-multiply p-8"
+                                src={image2}
+                                alt=""
+                            />
+                        </div>
+                    </div>
+                    <div className="hslide">
+                        <div className="flex flex-row justify-center items-center">
+                            <p className="text-[#6C1770] p-8 font-bold text-2xl text-center max-w-md animate-fade animate-once animate-duration-[3000ms] animate-delay-0 animate-ease-linear animate-normal animate-fill-forwards">
+                                <span className="font-extrabold">
+                                    Closing the Scan <br />
+                                    <br />
+                                </span>
+                                Gently bring your attention back to your breath.
+                                Take a few deep breaths, gradually becoming
+                                aware of your surroundings. When you're ready,
+                                open your eyes.
+                            </p>
+                            <img
+                                className="w-[500px] mix-blend-multiply p-8"
+                                src={image2}
+                                alt=""
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default BodyScan;
