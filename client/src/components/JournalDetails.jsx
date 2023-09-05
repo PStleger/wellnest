6bdfefb6f4faf71cd8566885ac86afea402e25f1
Phoebe St Leger-Davey <phoebestld@gmail.com>
2023-09-05 09:30:11 +0100

    journalsdone

--- client/src/components/JournalDetails.jsx
+++ client/src/components/JournalDetails.jsx
@@ -11,6 +11,10 @@ function formatDate(timestamp) {
   return `${day}/${month}/${year}`;
 }
 
+function sanitiseHTML(input) {
+  return input.replace(/<\/?[^>]+(>|$)/g, "");
+}
+
 const JournalDetails = () => {
   const [journals, setJournals] = useState([]);
   const [value, setValue] = useState("");
@@ -26,12 +30,13 @@ const JournalDetails = () => {
       .then((res) => {
         // Update the state with the retrieved journal data
         setJournals(res.data);
-        setEditedText(res.data.text); // Initialize editedText with the current text
+        setEditedText(sanitiseHTML(res.data.text)); // Initialize editedText with the current text
         console.log(res.data);
       })
       .catch((e) => console.log(e));
   }, [value]); // The effect will re-run whenever 'value' changes
-console.log(`${id}`);
+
+
   const handleDeleteJournal = () => {
     console.log("deletion is being handled");
     axios
@@ -62,20 +67,23 @@ console.log(`${id}`);
           <div className="tab-content tab-space">
             <div className="flex justify-around items-center flex-col-reverse gap-10 md:flex-row">
               <div className="h-auto w-2/3 xl:w-1/2 bg-[#EFE2F0]/50 rounded-3xl flex flex-col items-center justify-around p-10">
-                <div className="gridItem flex items-center justify-center gap-2 flex-wrap lg:mx-16 flex-col">
+                <div className="gridItem flex items-center justify-center flex-wrap lg:mx-16 flex-col">
                   <h2 className="text-[#6C1770] text-2xl py-6">
                     Journal Entry: {formatDate(journals.createdAt)}
                   </h2>
-                  <p>{journals.title}</p>
+                  <p className="text-xl pb-5">{journals.title}</p>
                   <p>{journals.description}</p>
 
+
+
+
                   {isEditing ? (
-                    <div>
-                      <textarea
+                    <div className="grid">
+                      <textarea className="rounded-2xl bg-transparent border-2 border-white/20 px-12 pt-5 mx-auto w-full h-72"
                         value={editedText}
                         onChange={(e) => setEditedText(e.target.value)}
                       />
-                     
+                     <div className="flex justify-center items-center flex-col md:flex-row md:gap-5">
                       {/* SAVE BUTTON */}
                       <Link
                         onClick={() => {
@@ -83,36 +91,40 @@ console.log(`${id}`);
                           setIsEditing(false);
                           navigate(`../journals/${id}`);
                         }}
-                        to="../journals/${id}"
-                        className="relative inline-flex items-center justify-center p-4 px-5 py-3 mt-10 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-2xl shadow-xl group hover:ring-1 hover:ring-purple-500"
+                        className="relative inline-flex items-center justify-center flex-row w-fit p-4 px-5 py-3 mt-5 sm:mt-10 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-2xl shadow-xl group hover:ring-1 hover:ring-purple-500"
                       >
                         <span className="absolute inset-0 w-full h-full bg-gradient-to-br from-[#88dfee] via-purple-400 to-[#DFC6E0]"></span>
                         <span className="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-45 translate-x-24 bg-pink-500 rounded-full opacity-30 group-hover:rotate-90 ease"></span>
                         <span className="relative text-white">Save</span>
                       </Link>
+                      {/* CANCEL BUTTON */}
                       <Link
                        onClick={() => setIsEditing(false)}
-                        className="relative inline-flex items-center justify-center p-4 px-5 py-3 mt-10 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-2xl shadow-xl group hover:ring-1 hover:ring-purple-500"
+                        className="relative inline-flex items-center justify-center w-fit p-4 px-5 py-3 mt-5 sm:mt-10 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-2xl shadow-xl group hover:ring-1 hover:ring-purple-500"
                       >
                         <span className="absolute inset-0 w-full h-full bg-gradient-to-br from-[#88dfee] via-purple-400 to-[#DFC6E0]"></span>
                         <span className="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-45 translate-x-24 bg-pink-500 rounded-full opacity-30 group-hover:rotate-90 ease"></span>
                         <span className="relative text-white">Cancel</span>
                       </Link>
+                      </div>
                     </div>
                   ) : (
                     <div>
-                      <div className="mt-5 px-10 w-auto h-auto border-2 border-white/20 rounded-2xl">
+                    <div className="flex flex-col items-center">
+                      <div className="mt-5 px-10 py-5 w-[200px] md:w-[500px]  h-auto border-2 border-white/20 rounded-2xl ">
                         <p
                           dangerouslySetInnerHTML={{
                             __html: journals && journals.text,
                           }}
                         />
                       </div>
+                      
+                      <div className="flex space-x-5 ">
                       {/* EDIT BUTTON */}
                       <Link
                         onClick={() => {
                           setIsEditing(true);
-                          handleUpdateJournal();
+                          // handleUpdateJournal();
                         }}
                         to={`../journals/${id}`}
                         className="relative inline-flex items-center justify-center p-4 px-5 py-3 mt-10 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-2xl shadow-xl group hover:ring-1 hover:ring-purple-500"
@@ -121,10 +133,6 @@ console.log(`${id}`);
                         <span className="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-45 translate-x-24 bg-pink-500 rounded-full opacity-30 group-hover:rotate-90 ease"></span>
                         <span className="relative text-white">Edit</span>
                       </Link>
-                    </div>
-                  )}
-                </div>
-                <div className="flex space-x-5">
                   {/* DELETE BUTTON */}
                   <Link
                     onClick={handleDeleteJournal}
@@ -135,15 +143,19 @@ console.log(`${id}`);
                     <span className="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-45 translate-x-24 bg-pink-500 rounded-full opacity-30 group-hover:rotate-90 ease"></span>
                     <span className="relative text-white">Delete</span>
                   </Link>
+                </div>
                   {/* ADD ENTRY BUTTON */}
                   <Link
                     to="../journals/new"
-                    className="relative inline-flex items-center justify-center p-4 px-5 py-3 mt-10 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-2xl shadow-xl group hover:ring-1 hover:ring-purple-500"
+                    className="relative inline-flex items-center justify-center w-fit p-4 px-5 py-3 mt-10 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-2xl shadow-xl group hover:ring-1 hover:ring-purple-500"
                   >
                     <span className="absolute inset-0 w-full h-full bg-gradient-to-br from-[#88dfee] via-purple-400 to-[#DFC6E0]"></span>
                     <span className="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-45 translate-x-24 bg-pink-500 rounded-full opacity-30 group-hover:rotate-90 ease"></span>
                     <span className="relative text-white">Add Entry</span>
                   </Link>
+                    </div>
+                    </div>
+                  )}
                 </div>
               </div>
             </div>
