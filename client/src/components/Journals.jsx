2a07f3d53d5243201d2dea407797b3ef942c3e46
Phoebe St Leger-Davey <phoebestld@gmail.com>
2023-09-06 09:59:40 +0100

    calendardonealmost

--- client/src/components/Journals.jsx
+++ client/src/components/Journals.jsx
@@ -10,7 +10,8 @@ const Journals = () => {
   const [currentMonth, setCurrentMonth] = useState(dayjs());
   const [selectedDate, setSelectedDate] = useState(null); // Add selectedDate state
   const { id } = useParams();
-
+  
+  console.log(`${id}`);
   useEffect(() => {
     axios
       .get(`/journals/journals`)
@@ -63,7 +64,7 @@ const Journals = () => {
             Next Month &gt;
           </button>
         </div>
-        <div className="grid grid-cols-7 gap-1">
+        <div className="grid grid-cols-7 gap-1 w-full">
           {isLoading ? (
             <p>Loading...</p>
           ) : (
@@ -101,33 +102,38 @@ const Journals = () => {
             )
           )}
         </div>
+        {/* ADD ENTRY BUTTON */}
+        <Link
+                    to="../journals/new"
+                    className="relative inline-flex items-center justify-center w-fit p-4 px-5 py-3 mt-10 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-2xl shadow-xl group hover:ring-1 hover:ring-purple-500"
+                  >
+                    <span className="absolute inset-0 w-full h-full bg-gradient-to-br from-[#88dfee] via-purple-400 to-[#DFC6E0]"></span>
+                    <span className="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-45 translate-x-24 bg-pink-500 rounded-full opacity-30 group-hover:rotate-90 ease"></span>
+                    <span className="relative text-white">Add Entry</span>
+                  </Link>
       </div>
       {/* JOURNAL LIST */}
       {selectedDate && (
-          <div>
-            <h2 className="text-[#6C1770] text-xl pt-4">Journal Titles for {selectedDate.format("MMMM DD, YYYY")}</h2>
-            <ul>
-              {journalEntries.map((entry) => {
-                const entryDate = dayjs(entry.createdAt);
-                if (
-                  entryDate.date() === selectedDate.date() &&
-                  entryDate.isSame(selectedDate, "month") &&
-                  entryDate.isSame(selectedDate, "year")
-                ) {
-                  return (
-                    <div key={entry.id}>
-                   
-                      <div className="p-6 my-5 grid gap-8 lg:grid-cols-1 h-70 max-h-[800px] w-full rounded-xl bg-[#EFE2F0]/50">
-                        <div className="flex justify-between items-center mb-5 text-[#6C1770]/50 ">
-                          <span className="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded ">
-                          </span>
-                          <span className="text-sm">{entry.date}</span>
-                        </div>
-                        <h2 className="mb-2 text-2xl font-bold tracking-tight text-[#6C1770] ">
-                          <Link to={`/journals/journals/${id}`}>{entry.title}</Link>
-                        </h2>
-                        <p className="mb-5 font-light text-[#6C1770]/50 ">
-                          {entry.text?.slice(1, 500)}
+  <div>
+    <h2 className="text-[#6C1770] text-xl pt-4">Journal Entries for {selectedDate.format("DD MMMM YYYY")}</h2>
+    <ul>
+      {journalEntries.map((entry) => {
+        const entryDate = dayjs(entry.createdAt);
+        if (
+          entryDate.date() === selectedDate.date() &&
+          entryDate.isSame(selectedDate, "month") &&
+          entryDate.isSame(selectedDate, "year")
+        ) {
+          // Use slice to limit the text to 75 characters
+          const truncatedText = entry.text.slice(0, 75);
+          const displayText = entry.text.length > 75 ? truncatedText + '...' : truncatedText;
+
+          return (
+            <div key={entry.id} className="w-96">
+              <div className="p-6 my-5 grid gap-8 lg:grid-cols-1 h-70 max-w-[800px] w-full rounded-xl bg-[#EFE2F0]/50">
+
+                <p className="mb-5 font-light text-[#6C1770]/50">
+                  <p dangerouslySetInnerHTML={{ __html: displayText }} />
                         </p>
                         <div className="flex justify-between items-end ">
                           <a
@@ -135,7 +141,7 @@ const Journals = () => {
                             className="inline-flex items-end font-medium text-primary-600  hover:underline"
                           >
                             <Link
-                              to={`/journals/journals/${id}`}
+                              to={`/journals/${entry._id}`}
                               className="flex justify-center items-center"
                             >
                               Read More
@@ -148,7 +154,7 @@ const Journals = () => {
                                 <path
                                   fillRule="evenodd"
                                   d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                                  clip-rule="evenodd"
+                                  clipRule="evenodd"
                                 ></path>
                               </svg>{" "}
                             </Link>
