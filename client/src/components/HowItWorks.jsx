9da42ec97a51c602420a54b54da917976c59ef87
Keerthana-Prabhakar <109957611+Keerthana-Prabhakar@users.noreply.github.com>
2023-09-06 12:22:55 +0200

    buttons

--- client/src/components/HowItWorks.jsx
+++ client/src/components/HowItWorks.jsx
@@ -42,23 +42,28 @@ const HowItWorks = () => {
               </p>
 
               <Link
-                to="/register"
-                className="inline-flex items-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
-              >
-                Sign Up
-                <svg
-                  className="ml-2 -mr-1 w-5 h-5"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-              </Link>
+  to="/register"
+  className="bg-gradient-to-br from-[#88dfee] via-purple-400 to-[#DFC6E0] text-white relative items-center justify-center px-4 py-2 overflow-hidden font-medium rounded-xl shadow-xl "
+  style={{
+    textDecoration: 'none',
+    display: 'inline-block',
+    cursor: 'pointer',
+  }}
+>
+  Sign Up
+  {/* <svg
+    className="ml-2 -mr-1 w-5 h-5"
+    fill="currentColor"
+    viewBox="0 0 20 20"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fillRule="evenodd"
+      d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+      clipRule="evenodd"
+    ></path>
+  </svg> */}
+</Link>
             </div>
           </div>
         </section>
@@ -120,23 +125,28 @@ const HowItWorks = () => {
                 inner balance
               </p>
               <Link
-                to="/progress/new"
-                className="inline-flex items-center text-black bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
-              >
-                Get started
-                <svg
-                  className="ml-2 -mr-1 w-5 h-5"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-              </Link>
+  to="/progress/new"
+  className="bg-gradient-to-br from-[#88dfee] via-purple-400 to-[#DFC6E0] text-white relative items-center justify-center px-4 py-2 overflow-hidden font-medium rounded-xl shadow-xl "
+  style={{
+    textDecoration: 'none',
+    display: 'inline-block',
+    cursor: 'pointer',
+  }}
+>
+  Get Started
+  {/* <svg
+    className="ml-2 -mr-1 w-5 h-5"
+    fill="currentColor"
+    viewBox="0 0 20 20"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fillRule="evenodd"
+      d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+      clipRule="evenodd"
+    ></path>
+  </svg> */}
+</Link>
             </div>
             <div className="mt-4 md:mt-0">
               <img
